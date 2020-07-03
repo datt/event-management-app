a26750515dabe0ffcea5347472a5f7060e4a3d20
Datta Dongare <datt@cart91.com>
2020-07-03 23:03:39 +0530

    Removed unused library bootstrap datepicker by tampus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap")
-require("tempusdominus-bootstrap-4")
 require("jquery")
 require("packs/custom")
 import "flatpickr/dist/flatpickr.min.css"
